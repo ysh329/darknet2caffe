160b88cb4fe886341aaf3fd2fc4b05385474d455
Shuai Yuan <ysh329@sina.com>
2018-02-09 01:48:41 +0000

    add scripts

---
+++ convert_YOLOv2_fromDarknet2Caffe.sh
@@ -0,0 +1,5 @@
+python darknet2caffe.py \
+models/YOLOv2/yolo-voc.2.0.cfg \
+models/YOLOv2/yolo-voc.2.0.weights \
+models/YOLOv2/yolo-voc.2.0.prototxt \
+models/YOLOv2/yolo-voc.2.0.caffemodel 
---
+++ convert_tinyYOLO_fromDarknet2Caffe.sh
@@ -0,0 +1,5 @@
+python darknet2caffe.py \
+models/tiny-yolo-125/tiny-yolo-voc.cfg \
+models/tiny-yolo-125/tiny-yolo-voc.weights \
+models/tiny-yolo-125/tiny-yolo-voc.prototxt \
+models/tiny-yolo-125/tiny-yolo-voc.caffemodel 
