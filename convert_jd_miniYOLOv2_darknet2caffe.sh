e98c3eddb2af86820a00041cb4ddd8f8ea7b3923
SSSSSSSSSSS <ysh329@sina.com>
2018-02-14 11:17:29 +0800

    update sh

---
+++ convert_jd_miniYOLOv2_darknet2caffe.sh
@@ -0,0 +1,3 @@
+python darknet2caffe.py \
+./models/tiny-yolo-jd/JDminiYOLOv2.cfg \
+./models/tiny-yolo-jd/JDminiYOLOv2.weights
--- convert_tinyYOLO_fromDarknet2Caffe.sh
+++ convert_tinyYOLO_fromDarknet2Caffe.sh
@@ -1,5 +1,3 @@
 python darknet2caffe.py \
 models/tiny-yolo-125/tiny-yolo-voc.cfg \
-models/tiny-yolo-125/tiny-yolo-voc.weights \
-models/tiny-yolo-125/tiny-yolo-voc.prototxt \
-models/tiny-yolo-125/tiny-yolo-voc.caffemodel 
+models/tiny-yolo-125/tiny-yolo-voc.weights 
